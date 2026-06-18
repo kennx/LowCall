84e0e5cb5318509e430aab8146db861bc732f801
Kenn <zhangxjian@me.com>
2026-06-19 07:24:55 +0800

    feat: wire whitelist, settings, contactLookup into AppContainer

--- app/src/main/java/cc/niaoer/nocall/AppContainer.kt
+++ app/src/main/java/cc/niaoer/nocall/AppContainer.kt
@@ -1,14 +1,20 @@
 package cc.niaoer.nocall
 
 import android.content.Context
+import cc.niaoer.nocall.data.ContactLookup
 import cc.niaoer.nocall.data.RuleMatcher
 import cc.niaoer.nocall.data.db.AppDatabase
 import cc.niaoer.nocall.data.db.BlockRuleDao
 import cc.niaoer.nocall.data.db.CallLogDao
+import cc.niaoer.nocall.data.db.WhitelistDao
+import cc.niaoer.nocall.data.prefs.SettingsRepository
 
 class AppContainer(context: Context) {
     val database: AppDatabase = AppDatabase.create(context)
     val blockRuleDao: BlockRuleDao = database.blockRuleDao()
     val callLogDao: CallLogDao = database.callLogDao()
+    val whitelistDao: WhitelistDao = database.whitelistDao()
     val ruleMatcher: RuleMatcher = RuleMatcher()
+    val settingsRepository: SettingsRepository = SettingsRepository(context.applicationContext)
+    val contactLookup: ContactLookup = ContactLookup(context.applicationContext)
 }
